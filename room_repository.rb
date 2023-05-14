25fc14bb02966f6d7dd5a1511a3d51d1705e66c4
sabrina amaral <sabrinamaral@hotmail.com>
2023-05-14 19:20:25 -0300

    deleting some copy/pasted code

--- room_repository.rb
+++ room_repository.rb
@@ -11,21 +11,10 @@ class RoomRepository
   private
 
   def load_csv
-    CSV.foreach(@csv_file) do |row|
-      id = row[0].to_i
-      name = row[1]
-      cured = row[2] == 'true'
-
-      @patients << Patient.new(id: id, name: name, cured: cured)
-    end
-    @next_id = @patients.empty? ? 1 : @patients.last.id + 1
+    # TODO: make this method reusable for both repositories
   end
 
   def save_csv
-    CSV.open(@csv_file, 'wb', headers: true) do |csv|
-      @patients.each do |patient|
-        csv << [patient.id, patient.name, patient.cured]
-      end
-    end
+    # TODO: make this method reusable for both repositories
   end
 end
